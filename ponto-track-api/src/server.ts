45658a2fb2d14041417a0d293312321be2c62842
GabrielDomingoss <gdomingoss11@gmail.com>
2024-05-27 02:34:21 -0400

    update api

--- ponto-track-api/src/server.ts
+++ ponto-track-api/src/server.ts
@@ -1,13 +1,18 @@
 import fastify from "fastify";
-import { knex } from "./database";
 import { env } from "./env";
 import { usersRoutes } from "./routes/users";
+import cors from "@fastify/cors";
 
 const app = fastify();
 
+app.register(cors, {
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+});
+
 app.register(usersRoutes, {
-  prefix: 'users'
-})
+  prefix: "api/users",
+});
 
 app
   .listen({
